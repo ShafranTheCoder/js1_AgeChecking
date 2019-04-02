f9584f882756dccdbdf434bb027daf0f0e8cffc7
Dmitry Bogutskii <bogutskii.d@gmail.com>
2019-04-02 14:13:17 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,50 @@
+'use strict';
+let name = 'Dima';
+let age = 18;
+let isConfirmed;
+
+function askName() {
+    while (true) {
+        name = prompt('Whats your name?', `${name}`);
+        if (name.replace(/[a-zA-Zа-яА-Я]/g,"").length || name === "") {
+            alert('Please write only letters!');
+        } else return;
+    }
+}
+function askAge() {
+    while (true) {
+        age = prompt('How old are you?', `${age}`);
+        if (age.replace (/[^a-zA-Zа-яА-Я]/g, '').length || age === "" || age[0] === '0') {
+            alert ('Please write only numbers!');
+        } else return;
+    }
+}
+function ageChecking() {
+    while (true) {
+        if (age < 5) {
+            alert('Write your real age!');
+            askAge();
+        } else if (age >=5 && age < 18) {
+            alert('You are not allowed to visit this website!');
+            return;
+        } else if (age >= 18 && age <= 22) {
+            isConfirmed = confirm('Are you sure you want to continue?');
+            if (isConfirmed) {
+                alert(`Welcome, ${name}!`);
+                return;
+            } else {
+                alert('You are not allowed to visit this website.');
+                return;
+            }
+        } else if (age > 22 && age <=85) {
+            alert(`Welcome, ${name}`);
+            return;
+        } else if (age > 85) {
+            alert (`Good for you, ${name}!`);
+            return;
+        }
+    }
+}
+askName();
+askAge();
+ageChecking();
