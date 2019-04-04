177a1aacaa14a993bed355951d812bb1b3d68202
Dmitry Bogutskii <bogutskii.d@gmail.com>
2019-04-04 16:32:17 +0300

    Fixed jshw1

--- js/main.js
+++ js/main.js
@@ -4,27 +4,32 @@ let age = 18;
 let isConfirmed;
 
 function askName() {
-    while (true) {
+    while (name) {
         name = prompt('Whats your name?', `${name}`);
-        if (name.replace(/[a-zA-Zа-яА-Я]/g,"").length || name === "") {
+        if (name.replace(/[a-zA-Zа-яА-Я]/g, "").length || name === "") {
             alert('Please write only letters!');
         } else return;
     }
 }
+
 function askAge() {
-    while (true) {
+    while (age) {
         age = prompt('How old are you?', `${age}`);
-        if (age.replace (/[^a-zA-Zа-яА-Я]/g, '').length || age === "" || age[0] === '0') {
-            alert ('Please write only numbers!');
+        if (age.replace(/[^a-zA-Zа-яА-Я]/g, '').length || age === "" || age[0] === '0') {
+            alert('Please write only numbers!');
         } else return;
     }
 }
-function ageChecking() {
-    while (true) {
+
+function main() {
+    askName();
+    askAge();
+
+    while (age && name) {
         if (age < 5) {
             alert('Write your real age!');
             askAge();
-        } else if (age >=5 && age < 18) {
+        } else if (age >= 5 && age < 18) {
             alert('You are not allowed to visit this website!');
             return;
         } else if (age >= 18 && age <= 22) {
@@ -36,15 +41,13 @@ function ageChecking() {
                 alert('You are not allowed to visit this website.');
                 return;
             }
-        } else if (age > 22 && age <=85) {
+        } else if (age > 22 && age <= 85) {
             alert(`Welcome, ${name}`);
             return;
         } else if (age > 85) {
-            alert (`Good for you, ${name}!`);
+            alert(`Good for you, ${name}!`);
             return;
         }
     }
 }
-askName();
-askAge();
-ageChecking();
+main();
